a7d1033d8036c2f03aababead9d80e43711a4e96
Joey <yty0606@gmail.com>
2021-11-01 14:33:51 +0800

    Feat: Second failing acceptance test
    Solution:
    1. As title.  Can't execute.
    2. Reference class 22.
    
    Author: Joey

--- OutsideInTDD/app/src/test/java/com/example/outsideintdd/acceptanceTests/CarFeature.kt
+++ OutsideInTDD/app/src/test/java/com/example/outsideintdd/acceptanceTests/CarFeature.kt
@@ -2,18 +2,26 @@ package com.example.outsideintdd.acceptanceTests
 
 import com.example.outsideintdd.Car
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 
 import org.junit.Test
 
 class CarFeature {
-
-    val car = Car(6.0)
+    private val engine = Engine()
+    val car = Car(6.0, engine)
 
     @Test
     fun carIsLoosingFuelWhenItTurnOn() {
-
         car.turnOn()
 
         assertEquals(5.5, car.fuel)
+    }
+
+    @Test
+    fun carIsTurningOnItEngineAndIncreaseTheTemperature() {
+        car.turnOn()
+
+        assertEquals(95, car.engine.temperature)
+        assertTrue(car.engine.isTurningOn)
     }
 }
