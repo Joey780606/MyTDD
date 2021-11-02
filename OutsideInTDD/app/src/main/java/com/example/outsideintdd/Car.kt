cf7f31b13bba86f112966b5efc25792dafb2123a
Joey <yty0606@gmail.com>
2021-11-02 17:56:41 +0800

    Feat: Coroutines practical example
    Solution:
    1. As title.  Real example.
    2. Reference class 33.
    
    Author: Joey

--- OutsideInTDD/app/src/main/java/com/example/outsideintdd/Car.kt
+++ OutsideInTDD/app/src/main/java/com/example/outsideintdd/Car.kt
@@ -1,10 +1,16 @@
 package com.example.outsideintdd
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 class Car(
     val engine: Engine,
     var fuel: Double) {
     fun turnOn() {
         fuel -= 0.5
-        engine.turnOn()
+        CoroutineScope(Dispatchers.Main).launch {
+            engine.turnOn()
+        }
     }
 }
--- OutsideInTDD/app/src/main/java/com/example/outsideintdd/MainActivity.kt
+++ OutsideInTDD/app/src/main/java/com/example/outsideintdd/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val engine = Engine()
+        val car = Car(engine, 20.0)
+
+        car.turnOn()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
