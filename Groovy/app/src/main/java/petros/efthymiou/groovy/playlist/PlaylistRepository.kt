3c3e7d91f98482a6dbf0a64f9cdad7688486ffce
Joey <yty0606@gmail.com>
2021-12-07 15:30:39 +0800

    Feat: Repository layer (Cont)
    Solution:
    1. As title.
    2. Reference class 57.
    
    Author: Joey

--- Groovy/app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistRepository.kt
+++ Groovy/app/src/main/java/petros/efthymiou/groovy/playlist/PlaylistRepository.kt
@@ -6,12 +6,5 @@ import kotlinx.coroutines.flow.flow
 class PlaylistRepository(
     private val service: PlaylistService
 ) {
-    suspend fun getPlaylists() : Flow<Result<List<Playlist>>> {
-        service.fetchPlaylists()
-
-        return flow {
-
-        }
-    }
-
+    suspend fun getPlaylists() : Flow<Result<List<Playlist>>> = service.fetchPlaylists()
 }
--- Groovy/app/src/test/java/petros/efthymiou/groovy/playlist/PlaylistRepositoryShould.kt
+++ Groovy/app/src/test/java/petros/efthymiou/groovy/playlist/PlaylistRepositoryShould.kt
@@ -3,12 +3,17 @@ package petros.efthymiou.groovy.playlist
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 import petros.efthymiou.groovy.utils.BaseUnitTest
 
 class PlaylistRepositoryShould : BaseUnitTest() {
     private val service : PlaylistService = mock()
+    private val playlists = mock<List<Playlist>>()
 
     @Test
     fun getsPlaylistsFromService() : Unit = runBlockingTest {
@@ -17,5 +22,23 @@ class PlaylistRepositoryShould : BaseUnitTest() {
         repository.getPlaylists()
 
         verify(service, times(1)).fetchPlaylists()
+    }
+
+    @Test
+    fun emitPlaylistsFromService(): Unit = runBlockingTest {
+        val repository = mockSuccessfulCase()
+
+        assertEquals(playlists, repository.getPlaylists().first().getOrNull())
+    }
+
+    private suspend fun mockSuccessfulCase(): PlaylistRepository {
+        whenever(service.fetchPlaylists()).thenReturn(
+            flow {
+                emit(Result.success(playlists))
+            }
+        )
+
+        val repository = PlaylistRepository(service)
+        return repository
     }
 }
